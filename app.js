2fe36ef67adfa00770e504be7c273bb6d072eeb1
clipeus-virtutis <henry_mcd_93@hotmail.com>
2022-02-14 12:39:45 +0000

    creates and solves a test for an invalid endpoint

--- app.js
+++ app.js
@@ -2,4 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.get("/api/topics" /*func name*/);
+
+app.all("/*", (req, res) => {
+  res.status(404).send({ msg: "Path not found" });
+});
+
 module.exports = app;
